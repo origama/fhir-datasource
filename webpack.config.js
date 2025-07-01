b5a5190dfaccb85be3b2d74c15b7fcd0bdba3219
origama <origama0@gmail.com>
2025-07-01 04:30:34 +0200

    Fix React dispatcher error by externalizing React

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     libraryTarget: 'amd'
   },
   externals: [
-    'lodash', 'moment',
+    'lodash', 'moment', 'react', 'react-dom',
     function (context, request, callback) {
       var prefix = 'grafana/';
       if (request.indexOf(prefix) === 0) {
